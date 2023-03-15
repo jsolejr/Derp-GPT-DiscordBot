777ebe95c65bc6f98db7fed74634596c05147343
jsolejr <jsolejr@gmail.com>
2023-03-15 03:24:12 -0500

    Update gpt4_bot.py.old.py
    Completely re-written for general chat bot functionality

--- gpt4_bot.py.old.py
+++ gpt4_bot.py.old.py
@@ -10,13 +10,11 @@ OPENAI_KEY = os.environ['OPENAI_KEY']
 
 openai.api_key = OPENAI_KEY
 
-# Create a bot instance with necessary intents
 intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 bot = commands.Bot(command_prefix=None, intents=intents)
 
-# Remove the default help command
 bot.remove_command("help")
 
 @bot.event
@@ -28,29 +26,19 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-#    response = openai.Completion.create(
-#        engine="gpt-4",
-#        prompt=message.content,
-#        max_tokens=150,
-#        n=1,
-#        stop=None,
-#        temperature=0.5,
-#    )
+    if bot.user in message.mentions:
+        response = openai.Completion.create(
+            engine="gpt-3.5-turbo",
+            prompt=message.content,
+            max_tokens=150,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    response = openai.Completion.create(
-    engine="gpt-3.5-turbo",
-    prompt=message.content,
-    max_tokens=150,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
+        response_text = response.choices[0].text.strip()
+        await message.channel.send(response_text)
 
-
-    response_text = response.choices[0].text.strip()
-    await message.channel.send(response_text)
-
-# Error handler to ignore CommandNotFound exception
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
